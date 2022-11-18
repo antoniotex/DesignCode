de5eea68ba6b3475ef582c250e90f056144366eb
Antonio Carlos <antoniotx.dev@gmail.com>
2022-11-18 02:06:47 -0300

    First commit

---
+++ DesignCode/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DesignCode
+//
+//  Created by Antonio Carlos de Moraes Teixeira on 18/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+            Text("UI Design")
+        }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Antonio Carlos de Moraes Teixeira on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
