857cd45120885bffc68661fd521868b0c8998ecd
Antonio Carlos <antoniotx.dev@gmail.com>
2022-11-26 15:16:15 -0300

    25 - If Show View and Transition

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State var show = false
     @State var viewState = CGSize.zero
-    @State var showCard = true
+    @State var showCard = false
     @State var bottomState = CGSize.zero
     @State var showFull = false
     
